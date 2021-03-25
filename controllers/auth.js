5a9f9a86218b026fde1ed273d0910858d7b7a466
Xsodex <flex513513@gmail.com>
2021-03-25 21:12:50 +0300

    Реализовали бэкэнд регистрации

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/User.js');
 
 
@@ -25,6 +26,21 @@ module.exports.register = async function (req, res) {
     } 
     else
     {
-        // Сохранить пользователя
+        // Шифрование пароля пользователя
+        const salt = bcrypt.genSaltSync(10);
+        const password = req.body.password;
+
+
+        const user = new User({
+            email: req.body.email,
+            password: bcrypt.hashSync(password, salt)
+        });
+
+        try {
+            await user.save();
+            res.status(201).json(user);
+        } catch (error) {
+            // Отправить ошибку
+        }
     }
 };
