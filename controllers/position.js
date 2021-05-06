7a20bfb538172ed162255799500c0493c4a61309
Xsodex <Flex513513@gmail.com>
2021-05-06 19:50:42 +0300

    Правки

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,17 @@
-const { json } = require('body-parser');
+const bodyParser = require('body-parser');
 const Position = require('../models/Position');
 const errorHandler = require('../Utils/errorHendler');
 
 // Контроллер для getByCategoryId(Получение всех позиций по Id категории)
 module.exports.getByCategoryId = async function (req, res) {
     try {
+        // Ищем в таблице позиции по 2 параметрам
         const positions = await Position.find({
             category: req.params.categoryId,
             user: req.user.id
         });
 
+        // Возвращаем пользователю позиции 
         res.status(200), json(positions);
     } catch (e) {
         errorHandler(res, e);
