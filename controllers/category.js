cfc4224526a0bfbdedb48da394bbe4fd6230dd70
Xsodex <Flex513513@gmail.com>
2021-05-06 20:45:17 +0300

    Контроллер категорий начало

--- controllers/category.js
+++ controllers/category.js
@@ -1,31 +1,73 @@
-// Контроллер для getAll
-module.exports.getAll = function (req, res) {
-    res.status(200).json({
-        message: "Это защищенная страница всех категорий. Доступ разрешен!"
-    });
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../Utils/errorHendler');
+
+
+
+
+
+
+// Контроллер для getAll(получить все категории который создал пользователь)
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({
+            user: req.user.id //Ищем категории по пользователю который их создал
+        });
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
 
 // Контроллер для getById
-module.exports.getById = function (req, res) {
-
+module.exports.getById = async function (req, res) {
+    try {
+        const categories = await Category.findById(req.params.id ); //Ищем категорию по id из переданных параметров
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
 
-// Контроллер для remove
-module.exports.remove = function (req, res) {
+// Контроллер для remove(Удалить категорию по id)
+module.exports.remove = async function (req, res) {
+    try {
+        await Category.remove({
+            _id: req.params.id //Удаляем категорию по id
+        });
 
+        await Category.remove({
+            category: req.params.id //Удаляем позиции из этой категории
+        });
+
+        // Возвращаем результат
+        res.status(200).json({
+            message: "Категория удалена"
+        });
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
 
 // Контроллер для create
 module.exports.create = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
 
 // Контроллер для update
 module.exports.update = function (req, res) {
+    try {
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 };
 
--- controllers/position.js
+++ controllers/position.js
@@ -2,47 +2,87 @@ const bodyParser = require('body-parser');
 const Position = require('../models/Position');
 const errorHandler = require('../Utils/errorHendler');
 
+
+
+
+
 // Контроллер для getByCategoryId(Получение всех позиций по Id категории)
 module.exports.getByCategoryId = async function (req, res) {
     try {
         // Ищем в таблице позиции по 2 параметрам
         const positions = await Position.find({
             category: req.params.categoryId,
-            user: req.user.id
+            user: req.user.id  //Эти данные берем из объекта user который добавил пасспорт в запрос
         });
 
         // Возвращаем пользователю позиции 
-        res.status(200), json(positions);
+        res.status(200).json(positions);
     } catch (e) {
         errorHandler(res, e);
     }
 };
 
 
-// Контроллер для remove
-module.exports.remove = function (req, res) {
+
+
+
+
+// Контроллер для remove(Удаляем позицию)
+module.exports.remove = async function (req, res) {
     try {
+        await Position.remove({
+            _id: req.params.id
+        });
 
+        // Возвращаем результат
+        res.status(200).json({
+            message: "Позиция была удалена"
+        });
     } catch (e) {
         errorHandler(res, e);
     }
 };
 
 
-// Контроллер для create
-module.exports.create = function (req, res) {
+
+
+
+
+// Контроллер для create(Создаем позицию)
+module.exports.create = async function (req, res) {
     try {
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save();
 
+        // Возвращаем пользователю позицию которую создали 
+        res.status(201).json(position);
     } catch (e) {
         errorHandler(res, e);
     }
 };
+
+
+
+
 
 
 // Контроллер для update
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
     try {
         
+        // Находим и обновляем позицию. 
+        const position = await Position.findOneAndUpdate(
+            {_id: req.params.id,}, //Ищем по id
+            {$set: req.body}, //Обновлять мы будем body запроса. В req.body находятся данные на которые будем менять старые
+            {new: true} //обновит позицию и верет нам уже обновленную
+        );
+
+        // Возвращаем пользователю обновленную позицию 
+        res.status(200).json(position);
     } catch (e) {
         errorHandler(res, e);
     }
