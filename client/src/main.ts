ca473f9473fd402ca95d0a04d30470e3c1d26ba0
Xsodex <Flex513513@gmail.com>
2021-06-17 20:00:13 +0300

    Proxy

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+// Скрипты materialize
+import "materialize-css/dist/js/materialize.js";
+
 if (environment.production) {
   enableProdMode();
 }
