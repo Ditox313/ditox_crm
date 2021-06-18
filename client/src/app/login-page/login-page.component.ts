135fe57dd2db899a1e3b3d8b79b20b6cab9d8d7f
Xsodex <Flex513513@gmail.com>
2021-06-18 18:58:05 +0300

    Сделал сервис авторизации

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,27 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { AuthService } from './../shared/services/auth.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form!: FormGroup;
+  uSub!: Subscription;
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
     });
+
+
+    this.route.queryParams.subscribe(function(params: Params){
+      if(params['registered'])
+      {
+        // Теперь вы можете зайти в систему используя свои данные
+      }
+      else if(params['accessDenied'])
+      {
+        // Сначала авторизируйтесь в системе
+      }
+    });
+  }
+
+
+
+  ngOnDestroy(){
+    if(this.uSub)
+    {
+      this.uSub.unsubscribe();
+    }
   }
 
 
   onSubmit(): void {
-    
+    this.form.disable();
+    const user = {
+      email: this.form.value.email,
+      password:  this.form.value.password
+    }
+
+    this.uSub = this.auth.login(user).subscribe(
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error);
+        this.form.enable();
+      }
+      
+    )
   }
 
 }
